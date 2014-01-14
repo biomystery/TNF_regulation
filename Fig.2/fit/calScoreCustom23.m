779a53e603c56b5a69e03e53b5f1128ff65dbb69
biomystery <zhangc518@gmail.com>
2014-01-14 10:30:48 -0800

    1. in the slides: I deleted the nascent data because I though the nascent and mRNA data aren’t match with each other.
    2. In Fig.2 Fit, calScoreCustom23. I found a bug: in simulating the
    tko, i used ode2 not ode23. I corrected and started to fit again.

--- Fig.2/fit/calScoreCustom23.m
+++ Fig.2/fit/calScoreCustom23.m
@@ -41,7 +41,7 @@ pars('kdeg_m') = kdeg(2);
 % tko 
 pars('k_pr') = k_pr_all(3);
 pars('kdeg_m') = kdeg(3);
-[~,tko]= ode15s(@ode2,times,yinit_all(:,3),[],[],nfkb_exp(:,[1 ...
+[~,tko]= ode15s(@ode23,times,yinit_all(:,3),[],[],nfkb_exp(:,[1 ...
                     4]),pars);
 
 % get date
@@ -70,7 +70,7 @@ residues(1) = (max_time(1) - 30)/5 ; % wt peak time
 residues(2) = (max_time(2) - 60)/10 ; % mko peak time 
 residues(3) = (max_time(3) - 30)/5 ; % tko peak time 
 
-residues(4) = (max_val(1) / max_val(2) - 0.68)/0.11*4; % peak_tko / peak_wt 
+residues(4) = (max_val(1) / max_val(2) - 0.68)/0.11; % peak_tko / peak_wt 
 residues(5) = (max_val(1) / max_val(3) - 0.75)/0.22; % peak_mko /peak_tko 
 
 %residues(6) = (simData_nascent(601,1)/mean([simData_nascent(601,2),simData_nascent(601,3)])- ...
@@ -86,7 +86,7 @@ residues(9) = (simData_nascent(1201,1)/simData_nascent(1201,3)-1.49)/.43;
 max_time = (max_ind-1)*0.1;
 
 residues(10) = (max_time(1) - 60)/10 ; % wt peak time 
-residues(11) = (max_time(2) - 60)/20 ; % mko peak time 
+residues(11) = (max_time(2) - 60)/10 ; % mko peak time 
 residues(12) = (max_time(3) - 60)/10 ; % tko peak time 
 
 residues(13) = (max_val(3) / max_val(1) - 0.73)/0.05; % peak_tko / peak_wt 
--- src/ode23_new.m
+++ src/ode23_new.m
@@ -1,9 +1,7 @@
 function dydt = ode23_new(t,x,options,nfkb_input,pars)
-    dydt = zeros(2,1);
+    dydt = zeros(1,1);
     kdeg_real = interp1([0 30 60 120],[0.07 pars('kdeg_m') 0.07 0.07],t);    
     nfkb = interp1(nfkb_input(:,1),nfkb_input(:,2),t);
     
-    dydt(1)=  pars('V_tr') * nfkb^pars('n') /(nfkb^pars('n') + pars('Km_tr')^pars('n')) - ...
-              pars('V_pr')*x(1)/(x(1) + pars('Km_pr')) ;
-    dydt(2)=  pars('V_pr')*x(1)/(x(1) + pars('Km_pr')) - kdeg_real*x(2) ;    
+    dydt(1)=  pars('V_tr') * nfkb^pars('n') /(nfkb^pars('n') + pars('Km_tr')^pars('n')) - kdeg_real*x(1) ;    
 end
