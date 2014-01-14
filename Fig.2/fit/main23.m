63ba491f2d3e17788b3e61eb08096665dac303c7
biomystery <zhangc518@gmail.com>
2014-01-14 14:50:31 -0800

    Increased penalty of abs(residuals) >1
    But still cannot fit the two sets of data at the same time. I think the
    data didn’t match is clear.

--- Fig.2/fit/main23.m
+++ Fig.2/fit/main23.m
@@ -1,12 +1,12 @@
 % read data from R 
 addpath('../../src/')
 
-%p = log10([0.5 0.6 1.5 1.5 3]);
+p = log10([0.5 0.6 1.5 1.5 3 1]);
 
 for i = 1:10
     p = parsFinal;
-    ub = log10([ 1 1 100 100 6 ]); % V_tr, Km_tr, k_pr, pr_fold
-    lb = log10([ 0.05 0.01 0.01 0.01 0.5]);
+    %ub = log10([ 1 1 100 100 6 ]); % V_tr, Km_tr, k_pr, pr_fold
+    %lb = log10([ 0.05 0.01 0.01 0.01 0.5]);
     ub = p + log10(5) ; lb =p - log10(5); 
     options =optimset('PlotFcns',{@optimplotx,@optimplotfunccount,@optimplotresnorm,@optimplotfirstorderopt},...
                       'TolFun',1e-10,'TolX',1e-10); 
